7f567ea8492a51b3690bcb6d49b4304c831d8111
2016025123 <ekfsla0113@hanyang.ac.kr>
2020-06-01 16:12:03 +0900

    add CafeModel for firebase

---
+++ WhichOneDo/WhichOneDo/Model/CafeModel.swift
@@ -0,0 +1,16 @@
+//
+//  CafeModel.swift
+//  WhichOneDo
+//
+//  Created by 강희선 on 2020/06/01.
+//  Copyright © 2020 강희선. All rights reserved.
+//
+
+import UIKit
+
+class CafeModel: NSObject {
+    @objc var cafeUid: String?
+    @objc var cafeName: String?
+    @objc var taste: String?
+    @objc var coordinate: String?
+}
