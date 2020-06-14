8b8cd2c5bd429cf51eb6154351387ccfefb14143
LimSungjun <juny9610@gmail.com>
2020-06-14 21:57:19 +0900

    #29 Implement community tableview

--- WhichOneDo/WhichOneDo/CommunityTableViewController.swift
+++ WhichOneDo/WhichOneDo/CommunityTableViewController.swift
@@ -1,90 +1,56 @@
-//
-//  CommunityTableViewController.swift
-//  WhichOneDo
-//
-//  Created by 임성준 on 2020/06/14.
-//  Copyright © 2020 강희선. All rights reserved.
-//
-
 import UIKit
 
-class CommunityTableViewController: UITableViewController {
 
+class CommunityTableViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet var communityTable: UITableView!
+    @IBOutlet var viewForTable: UIView!
+    @IBOutlet var viewForTable2: UIView!
+    @IBOutlet var communityTable2: UITableView!
+    let array1 = ["내가 쓴 글", "댓글 단 글", "스크랩"]
+    let array2 = ["자유게시판", "리뷰게시판", "정보게시판", "이벤트"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        communityTable2.dataSource = self
+        communityTable2.delegate =  self
+        communityTable2.separatorStyle = .none
+        viewForTable2.layer.cornerRadius = 30
+        viewForTable2.layer.borderWidth = 1.0
+        viewForTable2.layer.borderColor = UIColor(hex: "BFBFBF").cgColor
+        viewForTable2.clipsToBounds = true
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        communityTable.dataSource = self
+        communityTable.delegate = self
+        communityTable.separatorStyle = .none
+        viewForTable.layer.cornerRadius = 30
+        viewForTable.layer.borderWidth = 1.0
+        viewForTable.layer.borderColor = UIColor(hex: "BFBFBF").cgColor
+        viewForTable.clipsToBounds = true
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == self.communityTable{
+            return array1.count
+        }
+        else{
+            return array2.count
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+   
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell?
+        if tableView == self.communityTable{
+            cell = communityTable.dequeueReusableCell(withIdentifier: "noticeCell")
+            cell?.textLabel?.text = array1[indexPath.row]
+        }
+        else{
+            cell = self.communityTable2.dequeueReusableCell(withIdentifier: "noticeCell")
+            cell?.textLabel?.text = self.array2[indexPath.row]
+        }
+        return cell!
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
