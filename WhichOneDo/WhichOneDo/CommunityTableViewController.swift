a6abebc88010a38e9b224089baa3dd70b4a33ba5
LimSungjun <juny9610@gmail.com>
2020-06-21 17:19:58 +0900

    #29 Edit community table view and cellls & Implement write view

--- WhichOneDo/WhichOneDo/CommunityTableViewController.swift
+++ WhichOneDo/WhichOneDo/CommunityTableViewController.swift
@@ -1,56 +1,132 @@
+//
+//  CommunityTableViewController.swift
+//  WhichOneDo
+//
+//  Created by 임성준 on 2020/06/21.
+//  Copyright © 2020 강희선. All rights reserved.
+//
+
 import UIKit
 
+//Model에 추가하면 될 듯
+struct Community {
+    var boardTitle: String?
+    var title: String?
+    var time: String?
+    var userID: String?
+    var likeNum: String?
+    var commentsNum: String?
+}
 
-class CommunityTableViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
+var a = Community(boardTitle: "자유게시판", title: "안녕하세요", time: "12:34", userID: "커피입문자", likeNum: "0", commentsNum: "0")
+var b = Community(boardTitle: "리뷰게시판", title: "할리스커피 한양대점 리뷰", time: "20:47", userID: "카공족대표", likeNum: "4", commentsNum: "2")
+var c = Community(boardTitle: "정보게시판", title: "어떤 원두가 내 취향일까?", time: "04:04", userID: "원두감별사", likeNum: "17", commentsNum: "29")
+var d = Community(boardTitle: "이벤트", title: "스타벅스 여름 이벤트", time: "10:30", userID: "스타벅스 담당자", likeNum: "281", commentsNum: "99")
+
+var communityList : [Community] = [a, b, c, d]
+
+class CommunityTableViewController: UITableViewController {
+
+    @IBOutlet var communityListTable: UITableView!
+    @IBOutlet var btnWrite: UIBarButtonItem!
     
-    @IBOutlet var communityTable: UITableView!
-    @IBOutlet var viewForTable: UIView!
-    @IBOutlet var viewForTable2: UIView!
-    @IBOutlet var communityTable2: UITableView!
-    let array1 = ["내가 쓴 글", "댓글 단 글", "스크랩"]
-    let array2 = ["자유게시판", "리뷰게시판", "정보게시판", "이벤트"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        communityTable2.dataSource = self
-        communityTable2.delegate =  self
-        communityTable2.separatorStyle = .none
-        viewForTable2.layer.cornerRadius = 30
-        viewForTable2.layer.borderWidth = 1.0
-        viewForTable2.layer.borderColor = UIColor(hex: "BFBFBF").cgColor
-        viewForTable2.clipsToBounds = true
-
-        communityTable.dataSource = self
-        communityTable.delegate = self
-        communityTable.separatorStyle = .none
-        viewForTable.layer.cornerRadius = 30
-        viewForTable.layer.borderWidth = 1.0
-        viewForTable.layer.borderColor = UIColor(hex: "BFBFBF").cgColor
-        viewForTable.clipsToBounds = true
+        communityListTable.delegate = self
+        communityListTable.dataSource = self
         
+        let nibName = UINib(nibName: "CommunityTableViewCell", bundle: nil)
+        communityListTable.register(nibName, forCellReuseIdentifier: "communityCell")
+        
+        communityListTable.rowHeight = 100
+        
+        self.btnWrite.image = UIImage(named: "write")
+
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if tableView == self.communityTable{
-            return array1.count
-        }
-        else{
-            return array2.count
-        }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return communityList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "communityCell", for: indexPath) as! CommunityTableViewCell
+
+        cell.boardTitle.text = communityList[indexPath.row].boardTitle
+        cell.title.text = communityList[indexPath.row].title
+        cell.time.text = communityList[indexPath.row].time
+        cell.userID.text = communityList[indexPath.row].userID
+        cell.likeNum.text = communityList[indexPath.row].likeNum
+        cell.commentsNum.text = communityList[indexPath.row].commentsNum
+
+        return cell
     }
     
-   
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell: UITableViewCell?
-        if tableView == self.communityTable{
-            cell = communityTable.dequeueReusableCell(withIdentifier: "noticeCell")
-            cell?.textLabel?.text = array1[indexPath.row]
-        }
-        else{
-            cell = self.communityTable2.dequeueReusableCell(withIdentifier: "noticeCell")
-            cell?.textLabel?.text = self.array2[indexPath.row]
-        }
-        return cell!
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
     }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
     
-    
+    @objc func Write() {
+        let view = storyboard?.instantiateViewController(identifier: "write") as! WriteViewController
+        view.modalPresentationStyle = .fullScreen
+        self.present(view, animated: true, completion: nil)
+    }
+
 }
+
---
+++ WhichOneDo/WhichOneDo/WriteViewController.swift
@@ -0,0 +1,60 @@
+//
+//  WriteViewController.swift
+//  WhichOneDo
+//
+//  Created by 임성준 on 2020/06/21.
+//  Copyright © 2020 강희선. All rights reserved.
+//
+
+import UIKit
+
+class WriteViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet var btnDone: UIBarButtonItem!
+    @IBOutlet var contentsTitle: UITextField!
+    @IBOutlet var contents: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.black
+        
+        placeholerSetting()
+    
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func placeholerSetting() {
+        contents.delegate = self
+        contents.text = "내용을 입력하세요"
+        contents.textColor = UIColor.lightGray
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray{
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+            if textView.text.isEmpty {
+                textView.text = "내용을 입력하세요"
+                textView.textColor = UIColor.lightGray
+            }
+        }
+    
+}
+
+
